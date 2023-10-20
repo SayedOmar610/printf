f6e81005c46e634c9832bfbe8b0eea6415d37c47
sayedomar <sayeed.omeer22@gmail.com>
2023-10-20 07:22:30 -0700

    adding

--- main.h
+++ main.h
@@ -24,7 +24,7 @@ int printf_char(va_list va);
 int printf_string(va_list va);
 int printf_int(va_list args);
 int printf_precentage(void);
-int _printf(const char *format, ...);
 int _putchar(char c);
+int _printf(const char *format, ...);
 
 #endif
